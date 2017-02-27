6183c95eb864869e8636bfcacf624aebe88e4c1b
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-02-27 16:28:58 +0000

    adds more user route tests #36

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -3,6 +3,16 @@ defmodule Skillswheel.UserControllerTest do
   alias Skillswheel.User
 
   describe "all user paths that don't need authentication" do
+    setup do
+      %User{
+        id: 12345,
+        name: "My Name",
+        email: "email@test.com",
+        password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+      } |> Repo.insert
+      :ok
+    end
+
     test "/users/new", %{conn: conn} do
       conn = get conn, user_path(conn, :new)
       assert html_response(conn, 200) =~ "New User"
@@ -18,6 +28,26 @@ defmodule Skillswheel.UserControllerTest do
       conn = get conn, user_path(conn, :show, 123456)
       assert redirected_to(conn, 302) =~ "/"
       assert conn.halted
+    end
+
+    test "create new user", %{conn: conn} do
+      conn = post conn, user_path(conn, :create,
+      %{"user" => %{
+          name: "Test Name",
+          email: "test@test.com",
+          password: "secretpassword"
+        }})
+      assert redirected_to(conn, 302) =~ "/users"
+    end
+
+    test "Sign up fail: existing email", %{conn: conn} do
+      conn = post conn, user_path(conn, :create,
+      %{"user" => %{
+          name: "Test Name",
+          email: "email@test.com",
+          password: "secretpassword"
+        }})
+      assert html_response(conn, 200) =~ "New User"
     end
   end
 
