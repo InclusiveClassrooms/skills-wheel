2df0e6b469a999b5fe9e81f35573d7af930eabcc
Sam Houston <shouston3@me.com>
2017-02-28 08:59:09 +0000

    increasing the coverage up to 100%, #36

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -3,6 +3,7 @@ defmodule Skillswheel.ErrorViewTest do
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
+  import Skillswheel.ErrorHelpers
 
   test "renders 404.html" do
     assert render_to_string(Skillswheel.ErrorView, "404.html", []) ==
@@ -17,5 +18,13 @@ defmodule Skillswheel.ErrorViewTest do
   test "render any other" do
     assert render_to_string(Skillswheel.ErrorView, "505.html", []) ==
            "Internal server error"
+  end
+
+  test "translate_error function, empty opts" do
+    assert translate_error({"error", %{}}) == "error"
+  end
+
+  test "translate_error function opts with count" do
+    assert translate_error({"error", %{count: 1}}) == "error"
   end
 end
