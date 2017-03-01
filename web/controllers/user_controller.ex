9c892a1a856984706f538ee02324e07a39cd65bb
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-03-01 10:38:09 +0000

    removes schools dropdown from signup #50

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,7 +4,6 @@ defmodule Skillswheel.UserController do
   alias Skillswheel.School
 
   plug :authenticate_user when action in [:index, :show]
-  plug :load_schools when action in [:new, :index, :create, :show]
 
   def index(conn, _params) do
     users = Repo.all(User)
@@ -34,14 +33,5 @@ defmodule Skillswheel.UserController do
       render conn, "new.html", changeset: changeset
     end
   end
-
-  defp load_schools(conn, _) do
-    query =
-      School
-      |> School.alphabetical
-      |> School.names_and_ids
-    schools = Repo.all(query)
-    assign(conn, :schools, schools)
-  end
-
+  
 end
