03f0f23aca125af08d3a0504d482f741a2073206
Sam Houston <shouston3@me.com>
2017-03-02 11:20:53 +0000

    generating a random hash inside of , #60

--- web/controllers/forgotpass_controller.ex
+++ web/controllers/forgotpass_controller.ex
@@ -6,6 +6,8 @@ defmodule Skillswheel.ForgotpassController do
   end
 
   def create(conn, %{"forgotpass" => %{"email" => email}}) do
+    rand_string = gen_rand_string(30)
+
     email
     |> Skillswheel.Email.forgotten_password_email()
     |> Skillswheel.Mailer.deliver_now()
@@ -13,5 +15,11 @@ defmodule Skillswheel.ForgotpassController do
     conn
     |> put_flash(:info, "Email Sent")
     |> redirect(to: forgotpass_path(conn, :index))
+  end
+
+  defp gen_rand_string(length) do
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64()
+    |> binary_part(0, length)
   end
 end
