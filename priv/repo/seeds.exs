29e60194912a770825297f48b9c1d2d6ce9ca4de
Sam Houston <shouston3@me.com>
2017-03-06 15:12:08 +0000

    adding condition for adding Admin or not in seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,11 +13,15 @@
 alias Skillswheel.Repo
 alias Skillswheel.User
 
-Repo.insert! %User{
-  name: "Admin",
-  email: System.get_env("ADMIN_EMAIL"),
-  password: System.get_env("ADMIN_PASSWORD"),
-  password_hash: Comeonin.Bcrypt.hashpwsalt(System.get_env("ADMIN_PASSWORD")),
-  admin: true
-}
+case Repo.get_by(User, name: "Admin") do
+  {:ok, _user} -> IO.puts "Admin already in database"
+  _ -> 
+    Repo.insert! %User{
+      name: "Admin",
+      email: System.get_env("ADMIN_EMAIL"),
+      password: System.get_env("ADMIN_PASSWORD"),
+      password_hash: Comeonin.Bcrypt.hashpwsalt(System.get_env("ADMIN_PASSWORD")),
+      admin: true
+    }
+end
 
