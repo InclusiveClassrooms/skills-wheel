42733653415916c2b9977fa57d8729bf79a0959a
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-03-09 16:56:26 +0000

    adds more to the survey form #83

--- web/controllers/survey_controller.ex
+++ web/controllers/survey_controller.ex
@@ -29,28 +29,33 @@ defmodule Skillswheel.SurveyController do
   def show(conn, %{"id" => _student_id}, user) do
     changeset = Survey.changeset(%Survey{})
     form = [
-      %{ 
+      %{
         title: "Self Awareness & Self-Esteem",
         questions: [
           %{
             question_title: "Personal Appearance",
-            question_subtitle: "Are they able to identify at least three features of their personal appearance e.g. colour hair, colour eyes, height, clothing?"
+            question_subtitle: "Are they able to identify at least three features of their personal appearance e.g. colour hair, colour eyes, height, clothing?",
+            question_tag: "personal_appearance"
           },
           %{
             question_title: "Appearance of Others",
-            question_subtitle: "Are they able to identify at least three features of others’ personal appearance e.g. colour hair, colour eyes, height, clothing?"
+            question_subtitle: "Are they able to identify at least three features of others’ personal appearance e.g. colour hair, colour eyes, height, clothing?",
+            question_tag: "appearance_others"
           },
           %{
             question_title: "Likes",
-            question_subtitle: "Can they identify at least three things that they like?"
+            question_subtitle: "Can they identify at least three things that they like?",
+            question_tag: "likes"
           },
           %{
             question_title: "Dislikes",
-            question_subtitle: "Can they identify at least three things that they dislike?"
+            question_subtitle: "Can they identify at least three things that they dislike?",
+            question_tag: "dislikes"
           },
           %{
             question_title: "Strengths",
-            question_subtitle: "Can they identify something that they feel they are good at?"
+            question_subtitle: "Can they identify something that they feel they are good at?",
+            question_tag: "strengths"
           }
         ],
       },
@@ -59,24 +64,29 @@ defmodule Skillswheel.SurveyController do
         questions: [
           %{
             question_title: "Identify Emotions in Self",
-            question_subtitle: "Are they able to identify how they are feeling in different scenarios?"
+            question_subtitle: "Are they able to identify how they are feeling in different scenarios?",
+            question_tag: "identify_emotions_self"
           },
           %{
             question_title: "Identify Emotions in Others",
-            question_subtitle: "Are they able to identify how others might be feelings in different scenarios?"
+            question_subtitle: "Are they able to identify how others might be feelings in different scenarios?",
+            question_tag: "identify_emotions_others"
           },
           %{
             question_title: "Bodily Reactions to Emotions",
-            question_subtitle: "Can they explain how their body reacts when they feel different emotions e.g. when they are nervous their palms might sweat, they might go red, they might fidget?"
+            question_subtitle: "Can they explain how their body reacts when they feel different emotions e.g. when they are nervous their palms might sweat, they might go red, they might fidget?",
+            question_tag: "bodily_reaction_emotions"
           },
           %{
             question_title: "Identifies Appropriate Responses to Emotions",
-            question_subtitle: "Are they able to react appropriately to different emotions e.g. can they calm themselves down when they feel angry?"
+            question_subtitle: "Are they able to react appropriately to different emotions e.g. can they calm themselves down when they feel angry?",
+            question_tag: "identify_response_emotions"
           },
           %{
             question_title: "Follow a Plan to Respond to Emotions",
-            question_subtitle: "Can they appropriately follow a plan that they make to deal with a certain emotion e.g. breathe, count to ten and then talk to someone if they feel angry?
-          "}
+            question_subtitle: "Can they appropriately follow a plan that they make to deal with a certain emotion e.g. breathe, count to ten and then talk to someone if they feel angry?",
+            question_tag: "plan_respond_emotions"
+          }
         ],
       },
       %{
@@ -84,23 +94,28 @@ defmodule Skillswheel.SurveyController do
         questions: [
           %{
             question_title: "Good Eye Contact",
-            question_subtitle: "Do they follow the good eye contact rule when they talk to others?"
+            question_subtitle: "Do they follow the good eye contact rule when they talk to others?",
+            question_tag: "good_eye_contact"
           },
           %{
             question_title: "Good Distance/Touch",
-            question_subtitle: "Do they display appropriate distance and touch when they speak to someone?"
+            question_subtitle: "Do they display appropriate distance and touch when they speak to someone?",
+            question_tag: "good_distance_touch"
           },
           %{
             question_title: "Identify Facial Expressions",
-            question_subtitle: "Are they able to identify a range of different facial expressions in others?"
+            question_subtitle: "Are they able to identify a range of different facial expressions in others?",
+            question_tag: "identify_expressions"
           },
           %{
             question_title: "Identifies 'Good Body Language' in Self",
-            question_subtitle: "Can they identify when they are displaying good body language e.g. good distance, good touch and good eye contact?"
+            question_subtitle: "Can they identify when they are displaying good body language e.g. good distance, good touch and good eye contact?",
+            question_tag: "body_language_self"
           },
           %{
             question_title: "Identifies 'Good Body Language' in Others",
-            question_subtitle: "Can they identify when others are displaying good body language e.g. good distance, good touch and good eye contact?"
+            question_subtitle: "Can they identify when others are displaying good body language e.g. good distance, good touch and good eye contact?",
+            question_tag: "body_language_others"
           }
         ],
       },
@@ -109,24 +124,29 @@ defmodule Skillswheel.SurveyController do
         questions: [
           %{
             question_title: "Good Volume",
-            question_subtitle: "Do they speak with good volume, i.e. not too loudly or quietly?"
+            question_subtitle: "Do they speak with good volume, i.e. not too loudly or quietly?",
+            question_tag: "good_volume"
           },
           %{
             question_title: "Good Pace",
-            question_subtitle: "Do they speak with good pace, i.e. not too quickly or slowly?"
+            question_subtitle: "Do they speak with good pace, i.e. not too quickly or slowly?",
+            question_tag: "good_pace"
           },
           %{
             question_title: "Clarity",
-            question_subtitle: "Is their speech clear to others?"
+            question_subtitle: "Is their speech clear to others?",
+            question_tag: "clear_speech"
           },
           %{
             question_title: "Relevance",
-            question_subtitle: "Do they always speak with relevance in a conversation?"
+            question_subtitle: "Do they always speak with relevance in a conversation?",
+            question_tag: "speak_with_relevance"
           },
           %{
             question_title: "Identifies 'Good Speaking Rules'",
-            question_subtitle: "Are they able to identify how good speech sounds e.g. good volume, good pace, good clarity?
-          "}
+            question_subtitle: "Are they able to identify how good speech sounds e.g. good volume, good pace, good clarity?",
+            question_tag: "identify_good_speech"
+          }
         ],
       },
       %{
@@ -134,24 +154,29 @@ defmodule Skillswheel.SurveyController do
         questions: [
           %{
             question_title: "Thinks before Reacting",
-            question_subtitle: "Do they always think before they react to a problem in school?"
+            question_subtitle: "Do they always think before they react to a problem in school?",
+            question_tag: "think_before_react"
           },
           %{
             question_title: "Identifies Problems",
-            question_subtitle: "Do they understand when problems have occurred e.g. a fall out on the playground, an unforeseen change to timetabling, difficulties with peers’ friendships."
+            question_subtitle: "Do they understand when problems have occurred e.g. a fall out on the playground, an unforeseen change to timetabling, difficulties with peers’ friendships.",
+            question_tag: "understand_problems_occurred"
           },
           %{
             question_title: "Identifies Examples of 'Good Problem Solving'",
-            question_subtitle: "Can they identify situations in which problems were solved appropriately?"
+            question_subtitle: "Can they identify situations in which problems were solved appropriately?",
+            question_tag: "problems_solved_appropriately"
           },
           %{
             question_title: "Devise a Plan to Solve a Problem",
-            question_subtitle: "Can they make a plan to solve a problem that they’re involved in e.g. an approach to dealing with a change in timetabling?"
+            question_subtitle: "Can they make a plan to solve a problem that they’re involved in e.g. an approach to dealing with a change in timetabling?",
+            question_tag: "make_a_plan"
           },
           %{
             question_title: "Implement a Plan to Solve a Problem",
-            question_subtitle: "Can they follow a plan that they have devised to solve a problem that they’re involved in?
-          "}
+            question_subtitle: "Can they follow a plan that they have devised to solve a problem that they’re involved in?",
+            question_tag: "follow_a_plan"
+          }
         ],
       },
       %{
@@ -159,29 +184,41 @@ defmodule Skillswheel.SurveyController do
         questions: [
           %{
             question_title: "Built and Maintained One Friendship",
-            question_subtitle: "Do they have one school-based relationship with a peer that could be termed a friendship?"
+            question_subtitle: "Do they have one school-based relationship with a peer that could be termed a friendship?",
+            question_tag: "build_one_friendship"
           },
           %{
             question_title: "Built and Maintained Multiple Friendships",
-            question_subtitle: "Do they have more than one school-based relationship with a peer that could be termed a friendship?"
+            question_subtitle: "Do they have more than one school-based relationship with a peer that could be termed a friendship?",
+            question_tag: "build_multiple_friendships"
           },
           %{
             question_title: "Express Thoughts and Feelings to Others",
-            question_subtitle: "Are they able to tell others how they think and feel in an appropriate way in different situations?"
+            question_subtitle: "Are they able to tell others how they think and feel in an appropriate way in different situations?",
+            question_tag: "express_thoughts_to_others"
           },
           %{
             question_title: "Disagree with Others",
-            question_subtitle: "Can they appropriately say that they disagree with others’ opinions?"
+            question_subtitle: "Can they appropriately say that they disagree with others’ opinions?",
+            question_tag: "disagree_with_others"
           },
           %{
             question_title: "Apologise Appropriately",
-            question_subtitle: "Can they say sorry appropriately when it is expected of them?
-          "}
+            question_subtitle: "Can they say sorry appropriately when it is expected of them?",
+            question_tag: "apologise_appropriately"
+          }
         ]
       }
     ]
 
-    render conn, "show.html", form: form, changeset: changeset
+    emotions = [
+      "always",
+      "sometimes",
+      "rarely",
+      "never"
+    ]
+
+    render conn, "show.html", form: form, changeset: changeset, emotions: emotions
   end
 
   def action(conn, _) do
--- web/views/survey_view.ex
+++ web/views/survey_view.ex
@@ -1,3 +1,14 @@
 defmodule Skillswheel.SurveyView do
   use Skillswheel.Web, :view
+
+  def get_int_string(tuple) do
+    (elem(tuple, 1) + 1)
+    |> Integer.to_string
+  end
+
+  def atom_title(section) do
+    elem(section, 0).title
+    |> String.replace(" ", "-")
+    |> String.to_atom()
+  end
 end
