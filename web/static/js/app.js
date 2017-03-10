5b38952a7f2415e140961dff919741c9533226fc
Sam Houston <shouston3@me.com>
2017-03-10 00:11:48 +0000

    working survey with clickable radio button faces

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -37,7 +37,8 @@ function reverse_toggle(id) {
 export var App = {
   toggle_visibility: toggle_visibility,
   reverse_toggle: reverse_toggle,
-  mockWheel: mockWheel
+  mockWheel: mockWheel,
+  collapse: collapse
 }
 
 function mockWheel() {
@@ -87,3 +88,36 @@ function mockWheel() {
     '../images/'
   )
 }
+
+function collapse () {
+  var oneToSix = [1, 2, 3, 4, 5, 6];
+  var headingIdArr = oneToSix.map(function (num) { return "#heading-" + num});
+
+  function hiddenToggle(headingId) {
+    var section = "#section" + headingId.slice(8);
+    $(".glyphicon").addClass("glyphicon-chevron-down");
+    $(".glyphicon").removeClass("glyphicon-chevron-up");
+
+    if ($(section).hasClass("active")){
+      $(section).removeClass("active");
+      $(section).addClass("collapsed");
+    } else {
+      $(".section").addClass("collapsed");
+      $(".section").removeClass("active");
+      $(section).addClass('active');
+      $(section).removeClass('collapsed');
+      $(section + " .glyphicon").removeClass("glyphicon-chevron-down");
+      $(section + " .glyphicon").addClass("glyphicon-chevron-up");
+    }
+  }
+
+  (function expandListener(){
+    headingIdArr.forEach(function(sectionId) {
+      var section = document.querySelector(sectionId);
+      section.addEventListener('click', function() {
+        hiddenToggle(sectionId);
+      });
+    });
+  })();
+}
+
