4fac99733d0a2707511d3106f8b9210fb390f3c3
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-03-11 14:37:40 +0000

    adds styling for the registration/login page #96

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,8 +1,10 @@
 defmodule Skillswheel.SessionController do
   use Skillswheel.Web, :controller
+  alias Skillswheel.User
 
   def new(conn, _params) do
-    render conn, "new.html", layout: {Skillswheel.LayoutView, "login_layout.html"}
+    registration_changeset = User.changeset(%User{})
+    render conn, "new.html", layout: {Skillswheel.LayoutView, "login_layout.html"}, registration_changeset: registration_changeset
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
