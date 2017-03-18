e9ec09e8fac7d66effcd09a64dbe3bf238049d9b
Sam Houston <shouston3@me.com>
2017-03-18 11:13:01 +0000

    the admin template now recieves the correct data

--- web/controllers/admin_controller.ex
+++ web/controllers/admin_controller.ex
@@ -7,56 +7,6 @@ defmodule Skillswheel.AdminController do
   plug :authenticate_user when action in [:index]
   plug :authenticate_admin when action in [:index]
 
-  # Takes a list of all scores in the form:
-  # [[%{question: "...", answer: "1"}, ...],
-  #  [%{question; "...", answer: "2"}, ...]
-  #  ...
-  # ]
-  # And returns a list in the form:
-  # [[%{"Self Awareness & Self-Esteem" => 12},
-  #   %{"Managing Feelings" => 11},
-  #   %{"Non-Verbal Communication" => 10},
-  #   %{"Verbal Communication" => 4},
-  #   %{"Planning & Problem Solving" => 14},
-  #   %{"Relationships, Leaderships & Assertiveness" => 11}]
-  #  ], ...
-  # ]
-  # defp total_scores(surveys) do
-  #   survey_titles = [
-  #     "Self Awareness & Self-Esteem",
-  #     "Managing Feelings",
-  #     "Non-Verbal Communication",
-  #     "Verbal Communication",
-  #     "Planning & Problem Solving",
-  #     "Relationships, Leaderships & Assertiveness"]
-
-  #   surveys
-  #   |> Enum.map(fn survey -> survey
-  #     |> Map.new(fn {k, v} -> {k, String.to_integer v} end)
-  #     |> Enum.reduce(
-  #         [[]],
-  #         fn (q, acc) ->
-  #           if List.length(acc[List.length(acc) - 1]) === 5 do
-  #             acc ++ [[q]]
-  #           else
-  #             acc
-  #             |> Enum.with_index()
-  #             |> Enum.map(fn {a, i} ->
-  #               if i === a.length - 1 do
-  #                 a[a.length - 1] ++ [q]
-  #               else
-  #                 a
-  #               end
-  #             end)
-  #           end
-  #         end
-  #       )
-  #     |> Enum.map(fn grouping ->
-  #          Enum.reduce(grouping, 0, fn (n, acc) -> n + acc end)
-  #        end)
-  #   end)
-  # end
-
   def index(conn, _params) do
     schools = Repo.all(School)
     changeset = School.changeset(%School{})
@@ -84,8 +34,7 @@ defmodule Skillswheel.AdminController do
       |> Map.from_struct
       |> Map.drop([:updated_at, :inserted_at, :student,
                    :__meta__, :student_id, :id])
-      |> Map.values
-      |> Enum.map(&(String.to_integer(&1)))
+      |> Enum.map(fn {k, v} -> {k, String.to_integer(v)} end)
 
       ordered_survey = Survey.elems
       |> List.delete(:student_id)
@@ -105,6 +54,7 @@ defmodule Skillswheel.AdminController do
       |> Map.new
       |> Map.merge(%{"date" => date, "year" => year, "name" => name,
                      "group" => group_name, "ta" => ta, "school" => school})
+
     end)
 
     render conn, "index.html", changeset: changeset, schools: schools, surveys: surveys
