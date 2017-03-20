efbb40b977e2b9857e558739e29c5e11e6c7e5ed
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-03-20 15:05:05 +0000

    adds functionality to delete an approved school #129

--- web/controllers/school_controller.ex
+++ web/controllers/school_controller.ex
@@ -2,8 +2,8 @@ defmodule Skillswheel.SchoolController do
   use Skillswheel.Web, :controller
   alias Skillswheel.School
 
-  plug :authenticate_user when action in [:create]
-  plug :authenticate_admin when action in [:create]
+  plug :authenticate_user when action in [:create, :delete]
+  plug :authenticate_admin when action in [:create, :delete]
 
   def create(conn, %{"school" => school_params}) do
     changeset = School.changeset(%School{}, school_params)
@@ -15,6 +15,16 @@ defmodule Skillswheel.SchoolController do
       {:error, _changeset} ->
         conn
         |> put_flash(:error, "Failed to create school")
+        |> redirect(to: admin_path(conn, :index))
+    end
+  end
+
+  def delete(conn, %{"id" => school_id}) do
+    school = Repo.get(School, school_id)
+    case Repo.delete school do
+      {:ok, _struct}       ->
+        conn
+        |> put_flash(:info, "#{school.name} deleted!")
         |> redirect(to: admin_path(conn, :index))
     end
   end
